d44cbb02934b189c13aedc1557e432e452b2dccc
siddharthPabbisetty <68812061+Siddharth-Pabbisetty@users.noreply.github.com>
2025-09-03 01:22:31 +0530

    Update 1st_sept.java
    Updated the code

--- 1st_sept.java
+++ 1st_sept.java
@@ -1,37 +1,44 @@
-
+//Daily Problem 1st September
 class Solution {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        PriorityQueue<double[]> pq = new PriorityQueue<>(new Comparator<double[]>() {
+        PriorityQueue<double[]> pq = new PriorityQueue<>(new Comparator<double[]>() 
+                                                         {
             public int compare(double[] a, double[] b) {
-                if (a[0] < b[0]) return 1;
-                if (a[0] > b[0]) return -1;
+                if (a[0] < b[0]) 
+                    return 1;
+                if (a[0] > b[0]) 
+                    return -1;
                 return 0;
             }
         });
 
-        for (int i = 0; i < classes.length; i++) {
-            double pass = classes[i][0];
-            double total = classes[i][1];
-            double inc = (pass + 1.0) / (total + 1.0) - pass / total;
-            pq.offer(new double[]{inc, pass, total});
+        int n = classes.length;
+        for (int i = 0; i < n; i++) 
+        {
+            double p = classes[i][0];
+            double tot = classes[i][1];
+            double inc = (p + 1.0) / (tot + 1.0) - p / tot;
+            pq.offer(new double[]{inc, p, tot});
         }
 
-        while (extraStudents > 0) {
+        while (extraStudents > 0) 
+        {
             double[] top = pq.poll();
-            double pass = top[1] + 1;
-            double total = top[2] + 1;
-            double inc = (pass + 1.0) / (total + 1.0) - pass / total;
-            pq.offer(new double[]{inc, pass, total});
+            double p = top[1] + 1;
+            double tot = top[2] + 1;
+            double inc = (p + 1.0) / (tot + 1.0) - p / tot;
+            pq.offer(new double[]{inc, p, tot});
             extraStudents--;
         }
 
-        double sum = 0.0;
+        double ans = 0.0;
         Object[] arr = pq.toArray();
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) 
+        {
             double[] c = (double[]) arr[i];
-            sum += c[1] / c[2];
+            ans += c[1] / c[2];
         }
 
-        return sum / classes.length;
+        return ans/n;
     }
 }
