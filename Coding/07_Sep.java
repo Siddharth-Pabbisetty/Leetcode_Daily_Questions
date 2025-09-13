d77969d29d9a36c8be8aced71350bddda3a8d480
siddharthpabbisetty <sidhu.pabbis@gmail.com>
2025-09-13 10:03:29 +0530

    7th September problem added

---
+++ Coding/07_Sep.java
@@ -0,0 +1,11 @@
+//Daily Problem 7th September
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] ans = new int[n];
+        ans[0] = n * (1 - n) / 2;
+        for (int i = 1; i < n; ++i)
+            ans[i] = i;
+        return ans;
+    }
+}
