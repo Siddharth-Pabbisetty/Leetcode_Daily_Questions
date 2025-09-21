619213d89fa95f2679eaa4ddb436b4fd950d57c3
siddharthPabbisetty <68812061+Siddharth-Pabbisetty@users.noreply.github.com>
2025-09-22 01:09:18 +0530

    Update 13_Sep.java

--- Coding/13_Sep.java
+++ Coding/13_Sep.java
@@ -6,10 +6,12 @@ class Solution {
     public int maxFreqSum(String s) {
         Map<Character, Integer> mp = new HashMap<>();
         int c=0, v=0;
-        for (char ch : s.toCharArray()) {
+        for (char ch : s.toCharArray()) 
+        {
             mp.put(ch, mp.getOrDefault(ch, 0) + 1);
         }
-        for (Map.Entry<Character, Integer> i : mp.entrySet()) {
+        for (Map.Entry<Character, Integer> i : mp.entrySet()) 
+        {
             char ch = i.getKey();
             int cnt = i.getValue();
             if ("aeiou".indexOf(ch) >= 0)
