1f9b9dfca06dc279e36e1bc5dabc511495b6ecab
siddharthpabbisetty <sidhu.pabbis@gmail.com>
2025-09-24 23:20:17 +0530

    16th September problem added

---
+++ Coding/16_Sep.java
@@ -0,0 +1,32 @@
+//Daily Problem 16th September
+
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        List<Integer> st = new ArrayList<>();
+
+        for (int num : nums)
+        {
+            while (!st.isEmpty())
+            {
+                int top = st.get(st.size() - 1);
+                int g = gcd(top, num);
+                if (g == 1)
+                {
+                    break;
+                }
+                st.remove(st.size() - 1);
+                num = (top / g) * num;
+            }
+            st.add(num);
+        }
+
+        return st;
+    }
+
+    private int gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+}
