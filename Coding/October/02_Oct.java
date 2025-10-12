2eb38e9f42d22084c6c1ac1f267dcf2d7295412d
siddharthpabbisetty <sidhu.pabbis@gmail.com>
2025-10-12 22:52:58 +0530

    2nd October problem added

---
+++ Coding/October/02_Oct.java
@@ -0,0 +1,16 @@
+// Daily Problem 2nd October
+
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int d=numBottles;
+        int e=numBottles;
+
+        while(e>=numExchange){
+            e=(e*1)-numExchange;
+            numExchange++;
+            d=d+1;
+            e=e+1;
+        }
+        return d;
+    }
+}
