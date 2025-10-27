c52b9443b1323677b2165548314ee6e95a1f5fa1
siddharthPabbisetty <68812061+Siddharth-Pabbisetty@users.noreply.github.com>
2025-10-27 22:03:31 +0530

    Refactor maxBottlesDrunk method formatting

--- Coding/October/02_Oct.java
+++ Coding/October/02_Oct.java
@@ -1,10 +1,13 @@
 // Daily Problem 2nd October
 
-class Solution {
-    public int maxBottlesDrunk(int numBottles, int numExchange) {
+class Solution 
+{
+    public int maxBottlesDrunk(int numBottles, int numExchange) 
+    {
         int d=numBottles;
         int e=numBottles;
-        while(e>=numExchange){
+        while(e>=numExchange)
+        {
             e=(e*1)-numExchange;
             numExchange++;
             d=d+1;
