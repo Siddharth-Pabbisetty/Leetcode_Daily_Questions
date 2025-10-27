6a551ebc637ed4f8eac883203205725b8cab791e
siddharthPabbisetty <68812061+Siddharth-Pabbisetty@users.noreply.github.com>
2025-10-27 22:03:05 +0530

    Fix formatting and add new lines in 01_Oct.java

--- Coding/October/01_Oct.java
+++ Coding/October/01_Oct.java
@@ -1,7 +1,9 @@
 // Daily Problem 1st October
 
-class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
+class Solution 
+{
+    public int numWaterBottles(int numBottles, int numExchange) 
+    {
         return numBottles+(numBottles-1)/(numExchange-1);
     }
 }
