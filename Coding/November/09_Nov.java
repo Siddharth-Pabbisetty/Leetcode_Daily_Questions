2f47748cd0b75cf548453e5df626b97c3cc6a0fa
siddharthpabbisetty <sidhu.pabbis@gmail.com>
2025-11-20 23:02:57 +0530

    9th November problem added

---
+++ Coding/November/09_Nov.java
@@ -0,0 +1,14 @@
+// Daily Problem 9th November
+
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int cnt=0;
+        while(num1>0 && num2>0)
+        {
+            cnt+=num1/num2;
+            num1=num1%num2;
+            num1=num1+num2-(num2=num1);
+        }
+        return cnt;
+    }
+}
