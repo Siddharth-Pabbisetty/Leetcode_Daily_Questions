9900dcbb89f05da8f696c3bf37cf3823ae44819c
siddharthpabbisetty <sidhu.pabbis@gmail.com>
2025-11-21 21:46:49 +0530

    10th November problem added

---
+++ Coding/November/10_Nov.java
@@ -0,0 +1,20 @@
+// Daily Problem 10th November
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int[] st = new int[nums.length + 1];
+        int top=0;
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            while(st[top]>nums[i])
+            {
+                top--;
+                ans++;
+            }
+            if(st[top] != nums[i])
+                st[++top]=nums[i];
+        }
+        return ans+top;
+    }
+}
