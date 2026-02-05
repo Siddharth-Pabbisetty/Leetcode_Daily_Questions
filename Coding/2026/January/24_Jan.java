8f64c92f6ca3445ae07a06d9ce1f79b1ffbc2eda
siddharthpabbisetty <sidhu.pabbis@gmail.com>
2026-02-05 21:58:38 +0530

    24th January problem added

---
+++ Coding/2026/January/24_Jan.java
@@ -0,0 +1,15 @@
+// Daily Problem 24th January
+
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int res=0;
+        int n=nums.length;
+
+        for(int i=0;i < n>>1;i++)
+        {
+            res = Math.max(res, nums[n-1-i] + nums[i]);
+        }
+        return res;
+    }
+}
