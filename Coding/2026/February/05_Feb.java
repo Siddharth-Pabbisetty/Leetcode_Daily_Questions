51e0b3a89e088595e550c615d0847c15bd48cc03
siddharthpabbisetty <sidhu.pabbis@gmail.com>
2026-02-17 22:08:30 +0530

    5th February problem added

---
+++ Coding/2026/February/05_Feb.java
@@ -0,0 +1,13 @@
+// Daily Problem 5th February
+
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n=nums.length;
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=nums[(((i+nums[i])%n)+n)%n];
+        }
+        return a;
+    }
+}
