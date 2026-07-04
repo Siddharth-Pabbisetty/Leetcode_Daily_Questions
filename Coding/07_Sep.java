f17ec69228fa4ea5f500fb18c55f3796394390ef
siddharthPabbisetty <68812061+Siddharth-Pabbisetty@users.noreply.github.com>
2026-07-04 19:43:17 +0530

    Fix sumZero method to correctly populate array

--- Coding/07_Sep.java
+++ Coding/07_Sep.java
@@ -5,7 +5,9 @@ class Solution {
         int[] ans = new int[n];
         ans[0] = n*(1-n)/2;
         for (int i=1; i<n; ++i)
+        {
             ans[i] = i;
+        }
         return ans;
     }
 }
