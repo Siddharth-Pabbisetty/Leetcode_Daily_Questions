064f0e4a6dc11e825f877caaf5583bac342055c8
siddharthpabbisetty <sidhu.pabbis@gmail.com>
2026-08-19 23:07:31 +0530

    31st July problem added

---
+++ Coding/2026/July/31_July.java
@@ -0,0 +1,18 @@
+// Daily Problem 31st July
+
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq=new int[26];
+        for(char ch:word.toCharArray())
+        {
+            freq[ch-'a']++;
+        }
+        Arrays.sort(freq);
+        int res=0;
+        for(int i=25;i>=0 && freq[i]>0;i--)
+        {
+            res=res+freq[i]*((25-i)/8+1);
+        }
+        return res;
+    }
+}
